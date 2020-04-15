2cbaa8b4f8a87421a823ee135f976b9c65d7a002
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2020-04-15 21:08:49 +0200

    Added user info retrieval function
    Signed-off-by: Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>

--- initpw.sh
+++ initpw.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
+if test -f ".initpwconf" ; then
+    echo "File exists."
+else
+    echo "File doesn't exist."
+fi
+
+get_user_info(){
+    name=$(sed -n '1p' .initpwconf)
+    family_name=$(sed -n '2p' .initpwconf)
+    login=$(echo "${name}.${family_name}" | sed 's/\(.*\)/\L\1/')
+}
 
 link=http://www.debug-pro.com/epita/prog/s4/$(wget -qO- http://www.debug-pro.com/epita/prog/s4/ | grep "pw_0$1" | sed 's/^[^\"]*\"//g' | sed 's/\"[^\"]*$//')
 
-echo ${link}
+wget -qO- "${link}" > tmp
+
+make_authors(){
+    echo "${name}
+${family_name}
+${login}
+${login}@epita.fr" > AUTHORS
+}
