71396616dc19324372e7e6940ff2a2b326812648
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2020-04-15 23:02:23 +0200

    Can now clone repo
    Signed-off-by: Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>

--- initpw.sh
+++ initpw.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 get_user_info(){
-    if test -f ".initpwconf" ; then
-        name=$(sed -n '1p' .initpwconf)
-        family_name=$(sed -n '2p' .initpwconf)
+    if test -f "${HOME}/.initpwconf" ; then
+        echo "We're fine !!!"
+        name=$(sed -n '1p' "${HOME}/.initpwconf")
+        family_name=$(sed -n '2p' "${HOME}/.initpwconf")
         login=$(echo "${name}.${family_name}" | sed 's/\(.*\)/\L\1/')
     elif 1 ; then
         echo "File doesn't exist."
@@ -18,12 +19,22 @@ ${login}
 ${login}@epita.fr" > "$1AUTHORS"
 }
 
+init_git(){
+    gitcmd=$(grep "git clone" < tmp)
+    gitcmd="${gitcmd[*]/*git clone/git clone}"
+    gitcmd="${gitcmd[*]/john.smith/${login}}"
+    # eval "${gitcmd}"
+    ${gitcmd}
+}
+
 link=http://www.debug-pro.com/epita/prog/s4/$(wget -qO- http://www.debug-pro.com/epita/prog/s4/ | grep "pw_0$1" | sed 's/^[^\"]*\"//g' | sed 's/\"[^\"]*$//')
 title=${link[*]//*pw\//}
 title=${title[*]//\/index.html}
-mkdir "${title}"
 get_user_info
+wget -qO- "${link}" > tmp
+init_git
+cd "tp0$1-${login}" || exit 1
+mkdir "${title}"
 make_authors "${title}/"
 
-wget -qO- "${link}" > tmp
 
