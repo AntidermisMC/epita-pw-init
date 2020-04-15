5ceb8e57e22a54e985fbb6780d03bea3add2e8d2
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2020-04-15 17:42:21 +0200

    Get full link for practical page done
    Signed-off-by: Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>

--- initpw.sh
+++ initpw.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-wget -qO- http://www.debug-pro.com/epita/prog/s4/ | grep "pw_0$1"
+link=http://www.debug-pro.com/epita/prog/s4/$(wget -qO- http://www.debug-pro.com/epita/prog/s4/ | grep "pw_0$1" | sed 's/^[^\"]*\"//g' | sed 's/\"[^\"]*$//')
+
+echo ${link}
