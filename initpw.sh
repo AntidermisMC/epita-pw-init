de1777dcdc537b5f2447202c8aeefc9df47c6055
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2020-04-15 22:13:07 +0200

    Added main folder creation and moved AUTHORS file
    Signed-off-by: Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>

--- initpw.sh
+++ initpw.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env bash
-if test -f ".initpwconf" ; then
-    echo "File exists."
-else
-    echo "File doesn't exist."
-fi
 
 get_user_info(){
-    name=$(sed -n '1p' .initpwconf)
-    family_name=$(sed -n '2p' .initpwconf)
-    login=$(echo "${name}.${family_name}" | sed 's/\(.*\)/\L\1/')
+    if test -f ".initpwconf" ; then
+        name=$(sed -n '1p' .initpwconf)
+        family_name=$(sed -n '2p' .initpwconf)
+        login=$(echo "${name}.${family_name}" | sed 's/\(.*\)/\L\1/')
+    elif 1 ; then
+        echo "File doesn't exist."
+        exit 1
+    fi
 }
-
-link=http://www.debug-pro.com/epita/prog/s4/$(wget -qO- http://www.debug-pro.com/epita/prog/s4/ | grep "pw_0$1" | sed 's/^[^\"]*\"//g' | sed 's/\"[^\"]*$//')
-
-wget -qO- "${link}" > tmp
 
 make_authors(){
     echo "${name}
 ${family_name}
 ${login}
-${login}@epita.fr" > AUTHORS
+${login}@epita.fr" > "$1AUTHORS"
 }
+
+link=http://www.debug-pro.com/epita/prog/s4/$(wget -qO- http://www.debug-pro.com/epita/prog/s4/ | grep "pw_0$1" | sed 's/^[^\"]*\"//g' | sed 's/\"[^\"]*$//')
+title=${link[*]//*pw\//}
+title=${title[*]//\/index.html}
+mkdir "${title}"
+get_user_info
+make_authors "${title}/"
+
+wget -qO- "${link}" > tmp
+
